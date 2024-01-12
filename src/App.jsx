4c8821a6a1e45f5d07210aecc8d0786693f26bb4
Anupam Anand <136186030+anupam52@users.noreply.github.com>
2024-01-12 10:28:41 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './Components/CoreConcepts.jsx';
+import Examples from './Components/Examples.jsx';
+
+function App() {
+ 
+  return (
+    <>
+      <Header />
+      <main>
+      <CoreConcepts/>
+      <Examples/>
+       
+      </main>
+    </>
+  );
+}
+
+export default App;
